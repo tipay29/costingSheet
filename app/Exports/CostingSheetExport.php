ae04fbae9f6c1e885b17216c924b4ff1f5b7c4e3
warren <jwarren.hernandez@gmail.com>
2024-08-27 16:36:24 +0700

    ch header export excel

--- app/Exports/CostingSheetExport.php
+++ app/Exports/CostingSheetExport.php
@@ -7,6 +7,7 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Events\BeforeSheet;
+use PhpOffice\PhpSpreadsheet\Style\Border;
 
 class CostingSheetExport implements FromCollection,WithEvents
 {
@@ -64,27 +65,56 @@ class CostingSheetExport implements FromCollection,WithEvents
             },
             AfterSheet::class => function(AfterSheet $event){
                 $event->sheet->getColumnDimension('A')->setVisible(false);
-                $event->sheet->getColumnDimension('B')->setWidth(10);
+                $event->sheet->getColumnDimension('B')->setWidth(7);
+                $event->sheet->getColumnDimension('C')->setWidth(11);
+                $event->sheet->getColumnDimension('D')->setWidth(11);
+                $event->sheet->getColumnDimension('E')->setWidth(12);
+                $event->sheet->getColumnDimension('F')->setWidth(10);
+                $event->sheet->getColumnDimension('G')->setWidth(10);
+                $event->sheet->getColumnDimension('H')->setWidth(15);
+                $event->sheet->getColumnDimension('I')->setWidth(22);
+                $event->sheet->getColumnDimension('J')->setWidth(12);
+                $event->sheet->getColumnDimension('K')->setWidth(12);
+                $event->sheet->getColumnDimension('L')->setWidth(7);
+                $event->sheet->getColumnDimension('M')->setWidth(10);
+                $event->sheet->getColumnDimension('N')->setWidth(10);
+                $event->sheet->getColumnDimension('O')->setWidth(10);
+                $event->sheet->getColumnDimension('P')->setWidth(12);
+                $event->sheet->getColumnDimension('Q')->setWidth(12);
+                $event->sheet->getColumnDimension('R')->setWidth(12);
+                $event->sheet->getColumnDimension('S')->setWidth(12);
+                $event->sheet->getColumnDimension('T')->setWidth(15);
 
-                $event->sheet->setCellValue('B19','ITEM');
-                $event->sheet->setCellValue('C19','COMPONENT');
-                $event->sheet->setCellValue('D19','MATERIAL_ID');
-                $event->sheet->setCellValue('E19','1.FABRICS');
-                $event->sheet->setCellValue('F19','NOMINATED');
-                $event->sheet->setCellValue('G19','COO');
-                $event->sheet->setCellValue('H19','SUPPLIER_REF');
-                $event->sheet->setCellValue('I19','DESCRIPTION');
-                $event->sheet->setCellValue('J19','LOCATION');
-                $event->sheet->setCellValue('K19','MILL/SUPPLIER');
-                $event->sheet->setCellValue('L19','UOM');
-                $event->sheet->setCellValue('M19','WIDTH');
-                $event->sheet->setCellValue('N19','USAGE');
-                $event->sheet->setCellValue('O19','WASTAGE');
-                $event->sheet->setCellValue('P19','GROSS YIELD');
-                $event->sheet->setCellValue('Q19','UNIT COST');
-                $event->sheet->setCellValue('R19','HANDLING');
-                $event->sheet->setCellValue('S19','TOTAL');
-                $event->sheet->setCellValue('T19','COMMENT');
+                $borderall = [
+                    'borders' => [
+                        //outline all
+                        'outline' => [
+                            'borderStyle' => Border::BORDER_THIN,
+
+                        ],
+                    ],
+                ];
+                $event->sheet->setCellValue('B19','序号');
+                $event->sheet->getStyle('B19:T19')->applyFromArray($borderall)
+                    ->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('f2f205');
+                $event->sheet->setCellValue('C19','分类编号');
+                $event->sheet->setCellValue('D19','物料简码');
+                $event->sheet->setCellValue('E19','成本类别');
+                $event->sheet->setCellValue('F19','指定/非指定');
+                $event->sheet->setCellValue('G19','产地');
+                $event->sheet->setCellValue('H19','客户料号/供应商货号');
+                $event->sheet->setCellValue('I19','物料描述');
+                $event->sheet->setCellValue('J19','部位名称');
+                $event->sheet->setCellValue('K19','供应商');
+                $event->sheet->setCellValue('L19','单位');
+                $event->sheet->setCellValue('M19','规格');
+                $event->sheet->setCellValue('N19','用量');
+                $event->sheet->setCellValue('O19','损耗');
+                $event->sheet->setCellValue('P19','总用量');
+                $event->sheet->setCellValue('Q19','单价');
+                $event->sheet->setCellValue('R19','处理');
+                $event->sheet->setCellValue('S19','单价');
+                $event->sheet->setCellValue('T19','评论');
 
             },
         ];
